77fe6a50f6aee749c9837ca50cc735c69d0ca104
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2025-08-26 19:54:21 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,11 +15,11 @@ global.botNumber = ''
 
 global.owner = [
 // <-- Número @s.whatsapp.net -->
-  ['51919199620', '🜲 Propietario 🜲', true],
-  ['51969214380', 'shadow', true],
-  ['59898719147', 'feli', true],
-  ['51934053286', ':v', true ],
-  ['51946200884', ':v xd', true ],
+  ['573133374132', '🜲 Propietario 🜲', true],
+  ['573133374132', 'shadow', true],
+  ['573133374132', 'feli', true],
+  ['573133374132', ':v', true ],
+  ['573133374132', ':v xd', true ],
   
 // <-- Número @lid -->
 
