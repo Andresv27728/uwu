a88c6b4596a19a3a46225e30fc9743aa9b6d8d29
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2025-08-27 20:38:16 -0500

    Update info-estado.js

--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -3,7 +3,6 @@ let handler = async (m, { conn, usedPrefix, isRowner}) => {
 let _uptime = process.uptime() * 1000;
 let totalreg = Object.keys(global.db.data.users).length
 let totalchats = Object.keys(global.db.data.chats).length
-
 let uptime = clockString(_uptime);
 let users = [...new Set([...global.conns.filter((conn) => conn.user && conn.ws.socket && conn.ws.socket.readyState !== ws.CLOSED).map((conn) => conn)])];
 const chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
@@ -13,18 +12,19 @@ let old = performance.now()
 let neww = performance.now()
 let speed = neww - old
 const used = process.memoryUsage()
-let info = `▧ ═══『 ${botname} 』═══ ▧
-⟢ 🏆 𝐂𝐫𝐞𝐚𝐝𝐨𝐫: ${etiqueta}
-⟢ 🌀 𝐏𝐫𝐞𝐟𝐢𝐣𝐨: [ ${usedPrefix} ]
-⟢ 📌 𝐕𝐞𝐫𝐬𝐢𝐨́𝐧: ${vs}
-⟢ 💬 𝐂𝐡𝐚𝐭𝐬 𝐏𝐫𝐢𝐯𝐚𝐝𝐨𝐬: ${chats.length - groupsIn.length}
-⟢ 🗂 𝐂𝐡𝐚𝐭𝐬 𝐓𝐨𝐭𝐚𝐥𝐞𝐬: ${chats.length}
-⟢ 👥 𝐔𝐬𝐮𝐚𝐫𝐢𝐨𝐬: ${totalreg}
-⟢ 🏟 𝐆𝐫𝐮𝐩𝐨𝐬: ${groupsIn.length}
-⟢ ⏳ 𝐀𝐜𝐭𝐢𝐯𝐨: ${uptime}
-⟢ ⚡ 𝐕𝐞𝐥𝐨𝐜𝐢𝐝𝐚𝐝: ${(speed * 1000).toFixed(0) / 1000} seg
-⟢ 🤖 𝐒𝐮𝐛-𝐁𝐨𝐭𝐬 𝐀𝐜𝐭𝐢𝐯𝐨𝐬: ${totalUsers || '0'}
-▧ ═══════════════════ ▧
+let info = `🦈 ═══『 𝐆𝐀𝐖𝐑 𝐆𝐔𝐑𝐀 𝐔𝐋𝐓𝐑𝐀 』═══ 🦈
+⟢ 🌊 𝐂𝐫𝐞𝐚𝐝𝐨𝐫: Yo Soy Yo
+⟢ 🔱 𝐏𝐫𝐞𝐟𝐢𝐣𝐨: [ ${usedPrefix} ]
+⟢ 💎 𝐕𝐞𝐫𝐬𝐢𝐨́𝐧: ${vs}
+⟢ 🐟 𝐂𝐡𝐚𝐭𝐬 𝐏𝐫𝐢𝐯𝐚𝐝𝐨𝐬: ${chats.length - groupsIn.length}
+⟢ 🌊 𝐂𝐡𝐚𝐭𝐬 𝐓𝐨𝐭𝐚𝐥𝐞𝐬: ${chats.length}
+⟢ 🦈 𝐌𝐞𝐦𝐛𝐫𝐨𝐬 𝐄𝐬𝐜𝐮𝐞𝐥𝐚: ${totalreg}
+⟢ 🏝️ 𝐆𝐫𝐮𝐩𝐨𝐬 𝐀𝐜𝐮𝐚́𝐭𝐢𝐜𝐨𝐬: ${groupsIn.length}
+⟢ 🕐 𝐍𝐚𝐝𝐚𝐧𝐝𝐨 𝐩𝐨𝐫: ${uptime}
+⟢ 💨 𝐕𝐞𝐥𝐨𝐜𝐢𝐝𝐚𝐝 𝐝𝐞 𝐧𝐚𝐝𝐨: ${(speed * 1000).toFixed(0) / 1000} seg
+⟢ 🐠 𝐓𝐢𝐛𝐮𝐫𝐨𝐧𝐞𝐬 𝐀𝐜𝐭𝐢𝐯𝐨𝐬: ${totalUsers || '0'}
+🦈 ═════════════════════════ 🦈
+💙 "a! Todo funcionando perfectamente~" 💙
 `;
 await conn.sendFile(m.chat, banner, 'estado.jpg', info, m)
 }
@@ -32,9 +32,7 @@ handler.help = ['estado']
 handler.tags = ['info']
 handler.command = ['estado', 'status', 'estate', 'state', 'stado', 'stats']
 handler.register = true
-
 export default handler
-
 function clockString(ms) {
     let seconds = Math.floor((ms / 1000) % 60);
     let minutes = Math.floor((ms / (1000 * 60)) % 60);
