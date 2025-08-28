1fd20b446def83ca3e40a790c5181273aae79824
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2025-08-27 20:33:03 -0500

    Update info-ping.js

--- plugins/info-ping.js
+++ plugins/info-ping.js
@@ -1,23 +1,21 @@
 import speed from 'performance-now'
 import { spawn, exec, execSync } from 'child_process'
-
 let handler = async (m, { conn }) => {
          let timestamp = speed();
          let latensi = speed() - timestamp;
          exec(`neofetch --stdout`, (error, stdout, stderr) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
-
-          conn.reply(m.chat, `┏━❖『 ⚡ 𝐄𝐒𝐓𝐀𝐃𝐎 𝐎𝐍𝐋𝐈𝐍𝐄 』❖━┓
-┃ 🖤 *Sistema activo y estable.*
-┃ ⚡ 𝐓𝐢𝐞𝐦𝐩𝐨: ${latensi.toFixed(4)}ms
-┃ ❝  *! Pong ¡*❞
-┗━━━━━━━━━━━━━━━━━━━┛`, m, rcanal);
+          conn.reply(m.chat, `┏━❖『 🦈 𝐒𝐇𝐀𝐀𝐀𝐑𝐊 𝐒𝐓𝐀𝐓𝐔𝐒 』❖━┓
+┃ 💙 *Gura está nadando en aguas tranquilas~*
+┃ 🌊 𝐋𝐚𝐭𝐞𝐧𝐜𝐢𝐚: ${latensi.toFixed(4)}ms
+┃ 🦈 ❝ a! Sistema funcionando perfectamente ❞
+┃ 🐟 *¡El tiburón más rápido del océano!*
+┗━━━━━━━━━━━━━━━━━━━━━━━┛`, m, rcanal);
             });
 }
-handler.help = ['ping']
-handler.tags = ['info']
-handler.command = ['ping', 'p']
+handler.help = ['ping', 'shaaark', 'p']
+handler.tags = ['info', 'shark']
+handler.command = ['ping', 'p', 'shaaark', 'gura', 'a']
 handler.register = true
-
 export default handler
